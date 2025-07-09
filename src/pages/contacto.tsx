749449a32926a9e9b3a1e354256b6088550d6983
Lautaro Caballero <caballerolautarodev@gmail.com>
2025-07-09 18:20:20 -0300

    ⚒️ Contact page

--- src/pages/contacto.tsx
+++ src/pages/contacto.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
+import ContactForm from "../sections/ContactForm.tsx";
+
 const Contacto = () => {
     return (
-      <section>
-        <h1>Contacto</h1>
-        <p>Aquí irá el contenido de Contacto</p>
-      </section>
+      <div className="w-full">
+        <Header />
+        <ContactForm />
+        <Footer />
+      </div>
     );
   };
   
